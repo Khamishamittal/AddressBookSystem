87cca124b018bc67983d63ffd28432fbdd1290ab
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-22 21:00:13 +0530

    New Contact is added

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-        }
+            AddNewContact contact1 = new AddNewContact();
+
+            contact1.AddDetails();
+            contact1.ListingPeople();
+        
+    }
     }
 }
