5c523579864f86d946d5026ad9abc65ad513bc90
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-22 21:30:48 +0530

    Program to Edit Conatacts is Created

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace AddressBookSystem
+﻿using static AddressBookSystem.EditContacts;
+
+namespace AddressBookSystem
 {
     public class AddressBook
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            EditContacts person = new EditContacts();
+
+            person.AddDetails();
+
+
+
+            person.editContact();
+            person.ListingPeople();
         }
     }
 }
