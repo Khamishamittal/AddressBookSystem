6d0ccccffd0c0ce00bd3cf86595a099b583ceee1
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-22 21:45:10 +0530

    Program to Delete a Contact is created

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace AddressBookSystem
+﻿
+
+namespace AddressBookSystem
 {
     public class AddressBook
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            DeleteContact person = new DeleteContact();
+
+          
+            person.addContact();
+
+            person.deleteContact();
+            person.ListingPeople();
         }
     }
 }
