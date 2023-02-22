eb0a3fd446ed80d408912b904cc6292b381e4fa3
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-22 11:59:38 +0530

    Information of User get Created

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            CreateContacts contact = new CreateContacts("KHAMISHA", "Mittal", "kscsc", "dcdbsjbc", 7535, "bsjcb", 78449457557, "khamisha@gmail.com");
         }
     }
 }
