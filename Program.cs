9cbcb014cbb886afd843e3f5485721bffa8cdfbe
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-23 10:23:08 +0530

    Create a Program to add Multiple Contacts

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            CreateMultipleContacts person = new CreateMultipleContacts();
+
+            person.addContact();
+            person.ListingPeople();
         }
     }
-}
+    }
+
